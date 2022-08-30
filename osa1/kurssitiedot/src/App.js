bbc26b46701398fe33c143f1b0e5bef04d8acb95
boteemeli <eemeli.gustafsson@hotmail.com>
2022-08-30 12:41:43 +0300

    sum

---
+++ osa1/kurssitiedot/src/App.js
@@ -0,0 +1,51 @@
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        partone={part1}
+        exercisesone={exercises1}
+        parttwo={part2}
+        exercisestwo={exercises2}
+        parttre={part3}
+        exercisestre={exercises3}/>
+        <Total total={exercises1+exercises2+exercises3}/>
+    </div>
+  );
+};
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.partone} {props.exercisesone}
+      </p>
+      <p>
+        {props.parttwo} {props.exercisestwo}
+      </p>
+      <p>
+        {props.parttre} {props.exercisestre}
+      </p>
+    </>
+  );
+};
+const Total = (props) => {
+    return(
+        <>
+        <p>Number of exercises {props.total}</p>
+        </>
+    )
+};
+
+export default App;
